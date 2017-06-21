12894b921e2b02527dbb12696250b3f24b8bca66
Pedro Martins <palmelapedro@gmail.com>
2017-06-21 16:45:31 +0200

    Adding functionality to crossbarjs. Improved tests.

--- src/crossbar.js
+++ src/crossbar.js
@@ -1,12 +1,26 @@
 "use strict";
 
+const isString = require( "lodash.isstring" );
+const isFunction = require( "lodash.isfunction" );
 const autobahn = require( "autobahn" );
 
 const crossbarFacade = () => {
 
-    let connection;
+    const DEFAULT_OPTS = {
+        connect: {
+            "url": "ws://localhost:8080/ws",
+            "realm": "realm1"
+        },
+        publish: {},
+        subscribe: {},
+        call: {},
+        register: {}
+    };
 
-    const connect = function ( connectOpts ) {
+    let connection,
+        options = DEFAULT_OPTS;
+
+    const connect = function ( connectOpts = Object.assign( {}, DEFAULT_OPTS.connect ) ) {
         return new Promise( resolve => {
             connection = new autobahn.Connection( connectOpts );
             connection.onopen = () => resolve();
@@ -28,9 +42,31 @@ const crossbarFacade = () => {
         return connection;
     };
 
-    const registerRPCs = async function ( rpcList ) {
+    const register = function ( ...args ) {
+        const argsArray = Array.from( args );
+
+        if ( Array.isArray( argsArray[ 0 ] ) )
+            return registerMany( argsArray[ 0 ] );
+
+        if ( isString( argsArray[ 0 ] ) && isFunction( argsArray[ 1 ] ) && argsArray.length === 2 )
+            return registerOne( argsArray[ 0 ], argsArray[ 1 ] );
+
+        return Promise.reject( new Error( "Unrecognized parameters" ) );
+    };
+
+    const registerOne = function ( name, func ) {
+        if ( !isString( name ) )
+            throw new TypeError( `${name} must be a String.` );
+
+        if ( !isFunction( func ) )
+            throw new TypeError( `${func} must be a Function.` );
+
+        return getSession().register( name, func, options.register );
+    };
+
+    const registerMany = async function ( rpcList ) {
         for ( const rpc of rpcList ) {
-            await connection.session.register( rpc.name, rpc.func )
+            await registerOne( rpc.name, rpc.func )
                 .catch( err => {
                     throw new Error( `Failed to register "${rpc.name}":
                       ${JSON.stringify(err)}` );
@@ -38,12 +74,42 @@ const crossbarFacade = () => {
         }
     };
 
+    const call = function ( rpcName, ...args ) {
+        return getSession().call( rpcName, args, options.call );
+    };
+
+    const getOpts = function () {
+        return Object.assign( {}, options );
+    };
+
+    const setOpts = function ( newOpts ) {
+        options = newOpts;
+    };
+
+    const setOptsDefault = function () {
+        setOpts( DEFAULT_OPTS );
+    };
+
+    const publish = function ( topic, ...message ) {
+        return getSession().publish( topic, message, {}, options.publish );
+    };
+
+    const subscribe = function ( topic, callback ) {
+        return getSession().subscribe( topic, callback, options.subscribe );
+    };
+
     return Object.freeze( {
         connect,
         disconnect,
         getSession,
         getConnection,
-        registerRPCs
+        register,
+        call,
+        setOpts,
+        getOpts,
+        setOptsDefault,
+        publish,
+        subscribe
     } );
 };
 
---
+++ test/crossbar_spec.js
@@ -0,0 +1,178 @@
+"use strict";
+
+const chai = require( "chai" );
+const chaiAsPromised = require( "chai-as-promised" );
+chai.use( chaiAsPromised );
+const expect = chai.expect;
+
+const sinon = require( "sinon" );
+const xbarFacade = require( "../src/crossbar.js" );
+
+describe( "crossbarServer", () => {
+
+    const server = Object.assign( {}, xbarFacade() );
+
+    const rpcList = [ {
+        name: "hello",
+        func: () => "Hello World"
+    }, {
+        name: "add2",
+        func: args => args[ 0 ] + 2
+    }, {
+        name: "concat2",
+        func: args => args[ 0 ] + " " + args[ 1 ]
+    } ];
+
+    beforeEach( "reset crossbar state", () => {
+        server.setOptsDefault();
+    } );
+
+    it( "should connect to crossbar", done => {
+        server.connect()
+            .then( () => {
+                expect( server.getConnection().isConnected ).to.be.true;
+            } )
+            .then( done )
+            .catch( err => done( err ) );
+    } );
+
+    it( "should getOpts correctly", () => {
+        const DEFAULT_OPTS = {
+            connect: {
+                "url": "ws://localhost:8080/ws",
+                "realm": "realm1"
+            },
+            publish: {},
+            subscribe: {},
+            call: {},
+            register: {}
+        };
+        expect( server.getOpts() ).to.eql( DEFAULT_OPTS );
+    } );
+
+    it( "should setOpts correctly", () => {
+        server.setOpts( {} );
+        expect( server.getOpts() ).to.eql( {} );
+    } );
+
+    it( "should have an open connection after connecting", () => {
+        expect( server.getConnection().isOpen ).to.be.true;
+    } );
+
+    it( "should have a session from the connection", () => {
+        expect( server.getSession() ).to.not.equal( undefined );
+    } );
+
+    it( "should be able to register RPCs", done => {
+        server.register( rpcList )
+            .then( done )
+            .catch( done );
+    } );
+
+    it( "should be able to register one single rpc correctly", done => {
+        server.register( "singleRPC", () => {} )
+            .then( () => done() )
+            .catch( err => done( err ) );
+    } );
+
+    it( "should throw if one of the passed RPCs to register has an invalid name", () => {
+        const wrongRPCList = [ {
+            name: {},
+            func: () => {}
+        } ];
+
+        expect( server.register( wrongRPCList ) ).to.be.rejectedWith( Error );
+    } );
+
+    it( "should throw if one of the passed RPCs to register has an invalid function", () => {
+        const wrongRPCList = [ {
+            name: "failRPC",
+            func: "Not a function!"
+        } ];
+
+        expect( server.register( wrongRPCList ) ).to.be.rejectedWith( Error );
+    } );
+
+    it( "should be able to call an RPC with the correct parameters", () => {
+        const rpcName = "hello";
+        const callSpy = sinon.spy( server.getSession(), "call" );
+
+        server.call( rpcName );
+        expect( callSpy.calledWith( rpcName, [], {} ) ).to.be.true;
+        callSpy.restore();
+    } );
+
+    it( "should be able to call a registered RPC with no args", done => {
+        server.call( "hello" )
+            .then( response => {
+                expect( response ).to.eql( "Hello World" );
+                done();
+            } )
+            .catch( err => done( err ) );
+    } );
+
+    it( "should be able to call a registered RPC with an argument", done => {
+        server.call( "add2", 2 )
+            .then( response => {
+                expect( response ).to.eql( 4 );
+                done();
+            } )
+            .catch( err => done( err ) );
+    } );
+
+    it( "should be able to call a registered RPC with multiple argument", done => {
+        server.call( "concat2", "Hola", "Mundo" )
+            .then( response => {
+                expect( response ).to.eql( "Hola Mundo" );
+                done();
+            } )
+            .catch( err => done( err ) );
+    } );
+
+    it( "should throw if registering an RPC fails", () => {
+        expect( server.register( rpcList ) ).to.be.rejectedWith( Error );
+    } );
+
+    it( "should throw if register gets wrong parameters", () => {
+        const wrongParam = 0;
+        expect( server.register( wrongParam ) ).to.be.rejectedWith( Error );
+    } );
+
+    it( "should publish messages with one argument", () => {
+        const message = "Hello World";
+        const topic = "event";
+        const pubSpy = sinon.spy( server.getSession(), "publish" );
+        server.publish( topic, message );
+        expect( pubSpy.calledWith( topic, [ message ], {}, {} ) ).to.be.true;
+        pubSpy.restore();
+    } );
+
+    it( "should publish messages with several arguments", () => {
+        const args = [ "Hello", "World" ];
+        const topic = "event";
+        const pubSpy = sinon.spy( server.getSession(), "publish" );
+
+        server.publish( topic, ...args );
+        expect( pubSpy.calledWith( topic, args, {}, {} ) ).to.be.true;
+        pubSpy.restore();
+    } );
+
+    it( "should subscribe to topics", () => {
+        const topic = "TestTopic";
+        const callback = () => {};
+        const subSpy = sinon.spy( server.getSession(), "subscribe" );
+
+        server.subscribe( topic, callback );
+        expect( subSpy.calledWith( topic, callback, {} ) ).to.be.true;
+        subSpy.restore();
+    } );
+
+    it( "should disconnect from crossbar", done => {
+        server.disconnect()
+            .then( () => {
+                expect( server.getConnection().isConnected ).to.be.false;
+            } )
+            .then( done )
+            .catch( err => done( err ) );
+    } );
+} );
